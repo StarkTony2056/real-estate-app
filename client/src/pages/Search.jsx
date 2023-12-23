ec52d185a558990c8435b5d6e8983138809cd009
StarkTony2056 <chakrabortymainak03@gmail.com>
2023-12-23 19:29:10 +0530

    Added show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -14,8 +14,8 @@ export default function Search() {
         order: 'desc',
     });
     const [loading, setLoading] = useState(false);
-    const [listings, setListings] =useState([]);
-    console.log(listings);
+    const [listings, setListings] = useState([]);
+    const [showMore, setShowMore] = useState(false);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -49,9 +49,15 @@ export default function Search() {
 
         const fetchListings = async () => {
             setLoading(true);
+            setShowMore(false);
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            if(data.length > 8) {
+                setShowMore(true);
+            }else{
+                setShowMore(false);
+            }
             setListings(data);
             setLoading(false);
         };
@@ -93,6 +99,20 @@ export default function Search() {
         urlParams.set('order', sidebardata.order);
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`)
+    }
+
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if ( data.length < 9 ) {
+            setShowMore(false);
+        }
+        setListings([...listings, ...data]);
     }
 
     return (
@@ -150,6 +170,13 @@ export default function Search() {
                             <ListingItem key={listing._id} listing={listing} />
                         ))
                     }
+
+                    {showMore && (
+                        <button
+                            onClick={onShowMoreClick}
+                            className='text-green-700 hover:underline p-7 border border-green-700 rounded-lg'
+                        >Show more</button>
+                    )}
                 </div>
             </div>
         </div>
