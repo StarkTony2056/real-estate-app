e13b1484a2795c2c43653ca8d4715c16ed767f1a
StarkTony2056 <chakrabortymainak03@gmail.com>
2023-12-24 11:20:37 +0530

    fix 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,8 @@ export default function App() {
     <Header />
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/sign-in" element={<SignIn />}></Route>
+      <Route path="/sign-up" element={<SignUp />}></Route>
       <Route path="/about" element={<About />}></Route>
       <Route path="/search" element={<Search />}></Route>
       <Route path='/listing/:listingId' element={<Listing />}></Route>
@@ -25,8 +27,7 @@ export default function App() {
         <Route path="/create-listing" element={<CreateListing />}></Route>
         <Route path="/update-listing/:listingId" element={<UpdateListing />}></Route>
       </Route>
-      <Route path="/sign-in" element={<SignIn />}></Route>
-      <Route path="/sign-up" element={<SignUp />}></Route>
+      
       
     </Routes>
     </BrowserRouter>
